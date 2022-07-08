3400a85b0f08635beb10eedcd13743f8730c17e9
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-07-08 18:51:55 +0200

    event

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import Content from "./Content";
 
 function App() {
+	function hello() {
+		console.log("hello");
+	}
+
 	return (
 		<div className="title">
 			this is app
 			<Content />
+			<button onClick={hello}>Clique !!</button>
 		</div>
 	);
 }
