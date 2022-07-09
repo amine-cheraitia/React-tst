365795fc48fafa48107fe6640f0831992023ad76
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-07-09 03:33:55 +0200

    param props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import "./App.css";
 import Content from "./Content";
+import { useState } from "react";
 
 function App() {
+	let randomT = "teste";
+	const [monState, setMonState] = useState("before ");
+	console.log(monState, setMonState);
+
 	function hello() {
-		console.log("hello");
+		setMonState("after");
+	}
+	function toChild(param) {
+		console.log("salut " + param);
 	}
 
 	return (
 		<div className="title">
-			this is app
-			<Content />
-			<button onClick={hello}>Clique !!</button>
+			this is app {randomT}
+			<Content msg={monState} func={toChild} />
+			<button onClick={hello}>Clique !! </button>
+			{monState}
 		</div>
 	);
 }
--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./App.css";
 
-function Content() {
-	return <div>this is content</div>;
+/* function Content(props) {
+	return (
+		<div>
+			{" "}
+			<h5>this is content et ceci est une pros : {props.msg}</h5>{" "}
+			<button onClick={props.func}>Tochield</button>
+		</div>
+	);
+} */
+function Content({ msg, func }) {
+	function declanche() {
+		func("randomTest");
+	}
+
+	return (
+		<div>
+			<h5>le pros : {msg}</h5>{" "}
+			<button className="red" onClick={declanche}>
+				Tochield
+			</button>
+		</div>
+	);
 }
-
 export default Content;
